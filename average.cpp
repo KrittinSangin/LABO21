65dd76cf92c57231db405fd5ea117e006ac11aee
Perm <permsangin@gmail.com>
2025-02-18 16:29:58 +0700

    Finished all lab

--- average.cpp
+++ average.cpp
@@ -1,1 +1,17 @@
-
+#include<iostream>
+using namespace std;
+int main(int argc, char* argv[]){
+    float sum = 0;
+    if(argc == 1){
+        cout << "Please input numbers to find average.";
+    }else{
+        for(int i = 1; i < argc ; i ++){
+            
+            sum += atof(argv[i]);
+            
+        }
+        cout << "---------------------------------" << endl;
+        cout<< "Average of " << argc-1<<" numbers = " << sum/(argc-1) << endl;
+        cout << "---------------------------------";
+    }
+}
